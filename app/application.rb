accf1e4ad9c97ceeb4984dfd8916bbb340b5d3a2
Katleiah Ramos <katleiahramos@gmail.com>
2018-05-15 17:48:58 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,12 +6,12 @@ class Application
     resp = Rack::Response.new
     req = Rack::Response.new(env)
 
-    if req.path==/items
-        item_name = req.path.split(/items/).last
-        @@items.includes?(item_name) ? item = @@items.find{|i| i.name == item_name}.price : resp.status = 404
-    else
-      resp.status = 404
-    end
+    # if req.path==/items
+    #     item_name = req.path.split(/items/).last
+    #     @@items.includes?(item_name) ? item = @@items.find{|i| i.name == item_name}.price : resp.status = 404
+    # else
+    #   resp.status = 404
+    # end
   end
 
 end
