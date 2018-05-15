58eeb5c624416548c26e42a55c38507305157348
Katleiah Ramos <katleiahramos@gmail.com>
2018-05-15 17:57:35 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -10,7 +10,7 @@ class Application
     if req.path.match(/items/)
         item_name = req.path.split(/items/).last
 
-        @@items.includes?(item_name) ? item = @@items.find{|i| i.name == item_name}.price : resp.status = 404
+        @@items.include?(item_name) ? item = @@items.find{|i| i.name == item_name}.price : resp.status = 404
     else
       resp.write "Route not found"
       resp.status = 404
