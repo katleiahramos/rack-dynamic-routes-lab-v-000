1b275a076d323f31d7bcabf37e29e49548b4ab30
Katleiah Ramos <katleiahramos@gmail.com>
2018-05-15 17:51:12 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,13 +5,14 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-
-    if req.path=="/items/"
-        item_name = req.path.split(/items/).last
-        @@items.includes?(item_name) ? item = @@items.find{|i| i.name == item_name}.price : resp.status = 404
-    else
-      resp.status = 404
-    end
+    binding.pry
+    # 
+    # if req.path=="/items/"
+    #     item_name = req.path.split(/items/).last
+    #     @@items.includes?(item_name) ? item = @@items.find{|i| i.name == item_name}.price : resp.status = 404
+    # else
+    #   resp.status = 404
+    # end
   end
 
 end
